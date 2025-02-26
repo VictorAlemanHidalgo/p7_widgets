d04e5bf22766c9ca1e44b88843fa215e20c91341
VICTOR_ALEMAN_HIDALGO <a.22308051281123@cbtis128.edu.mx>
2025-02-26 00:13:42 +0000

    p7_widgets-VictorAleman

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiWidgets extends StatelessWidget {
+  const MiWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Alemán",
+                style: TextStyle(color: Color(0xffffffff))),
+            centerTitle: true,
+            backgroundColor: const Color(0xff6678dc),
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text(
+                'Texto en columna',
+                style: TextStyle(fontSize: 25, color: Colors.blue),
+              ),
+              Text(
+                'Mat: 22308051281123 Gpo 6 J',
+                style: TextStyle(fontSize: 20, color: Colors.purple),
+              ),
+              Expanded(
+                child: FittedBox(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          )),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } // build
+} // Mi widgets
